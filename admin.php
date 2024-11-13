6db2b9f0bcdabfc10e5def8c848b888d3013560c
PAKOOOOOOOOO-hub <zhabelovis@gmail.com>
2024-11-13 11:05:47 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,21 @@
+<? include("includes/db.php");
+$echo = "текст";
+?>
+
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Админка</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div class="wrapper">
+		<main class="main" id="main">
+			<?echo $echo;?>		
+		</main>
+	</div>
+</body>
+</html>
